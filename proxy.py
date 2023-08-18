30897627d9d5f2b2b356470e2557e080b1725193
HenryLePhat <121381070+HenryLePhat@users.noreply.github.com>
2023-08-18 19:40:02 +0700

    Add files via upload

--- proxy.py
+++ proxy.py
@@ -1,17 +1,20 @@
 from socket import *
-from datetime import datetime
+from datetime import datetime, timedelta
 from threading import *
-import webbrowser
+import time
+import os
 
+# Struct in config file
 class RequestConfig:
     def __init__(self, methods, whitelist, start_time, end_time, cache_types, cache_time):
-        self.methods = methods
-        self.whitelist = whitelist
-        self.start_time = start_time
-        self.end_time = end_time
-        self.cache_types = cache_types
-        self.cache_time = cache_time
-
+        self.methods = methods  # Methods
+        self.whitelist = whitelist # whitelist
+        self.start_time = start_time # Start Time
+        self.end_time = end_time    # End Time
+        self.cache_types = cache_types # Types of caches
+        self.cache_time = cache_time # Time to reload caches
+        
+# Function to read config file
 def parse_request_config(file_path):
     config_data = {}
     with open(file_path, "r") as file:
@@ -25,34 +28,43 @@ def parse_request_config(file_path):
     time_range = config_data.get("Time", "").strip()
     start_time = ""
     end_time = ""
+    
+    # Read time
     if "-" in time_range:
         time_parts = [part.strip() for part in time_range.split('-')]
         if len(time_parts) == 2:
             start_time, end_time = time_parts
+    # Read cache types
     cache_types = [cache.strip() for cache in config_data.get("Cache_type", "").split(',')]
+    #Read caches time
     cache_time = config_data.get("Cache_time", "")
 
     # Create and return a RequestConfig instance
     return RequestConfig(methods, whitelist, start_time, end_time, cache_types, cache_time)
 
+# Fuction to delete caches 
+def releaseCache(caches):
+    while True:
+        current_time = datetime.now()
+        for cache in caches:
+            if current_time > cache['time']:
+                os.remove('_pycache_/' + cache['name'])
+        time.sleep(30)
 
+# Function to validate request
 def Validate(request, config, message):
     # Empty request
     if not request:
         message[0] = 'Empty request'
-        print(1, end='\n')
         return False
 
     strRequest = request.decode()
     # ["GET", "/path", "HTTP/1.1", "Host:", "example.com"]
-
+    
     # Method
-
     method = strRequest.split()[0]
     if method not in config.methods:
         message[0] = 'Invalid method'
-        print(2, end='\n')
-        # print(method, end='\n')
         return False
 
     # Whitelist
@@ -61,9 +73,7 @@ def Validate(request, config, message):
         hostn = hostn.replace('www.', '')
 
     if hostn not in config.whitelist:
-        message[0] = 'You are not allowed to access this page'
-        print(3, end='\n')
-        # print(hostn, end='\n')
+        message[0] = 'You are not allowed to access this page !!!'
         return False
 
     # Time
@@ -72,12 +82,8 @@ def Validate(request, config, message):
     mins_now = now.split(":")[1]
     now = hour_now + ":" + mins_now
     if now < config.start_time or now > config.end_time:
-        message[0] = 'You are not allowed to access this page at this moment'
-        print(4, end='\n')
-        # print(now, config.start_time, config.end_time)
-        # print(end='\n')
+        message[0] = 'You are not allowed to access this page at this moment !!!'
         return False
-    print(5, end='\n')
     # Accept
     return True
 
@@ -87,54 +93,88 @@ file_path = "config.txt"
 # Parse the configuration and create a RequestConfig instance
 request_config = parse_request_config(file_path)
 
-# Simulating a request
-request = b"POS /path HTTP/1.1\r\nHost: example.com\r\n\r\n"
+# Function to send 403 error
+def handleForbidden():
+    with open("error.html", 'r') as file:
+        error = file.read()
+    errorRes = f"HTTP/1.1 403 Forbidden\r\n\r\n{error}"
+    return errorRes.encode()
 
-# Placeholder for the error message
-error_message = [None]
-
-# Validate the request using the configuration
-# is_valid = Validate(request, request_config, error_message)
-
-def ProcessProxy(client):
-    req = client.recv(4096)
+# Connect
+def ProcessProxy(client, caches):
+    # Get request from client
+    req = client.recv(2 ** 20)
+    #Validate
     message = ['']
     if (Validate(req, request_config, message)):
-         if (req.decode().split()[0] != "CONNECT"):
-            print('Sending request for', req.decode().split()[1])
+        if (req.decode().split()[0] != "CONNECT"):
+            print('Sending Request for', req.decode().split()[1])
 
             # example.com
             pathName = req.decode().split()[1]
-            pathName = pathName.replace('http://', '')
-            pathName = pathName.replace('/', '')
-            print('Path: ', req.decode().split()[4], end='\n')
-
-            addr = req.decode().split()[4]
-            if not addr.startswith('www.'):
-                addr = 'www.' + addr
-            print(addr, end='\n')
-            s = socket(AF_INET,SOCK_STREAM)
-            s.connect((addr, 80))
-            s.send(req)
+            cacheName = pathName.replace('http://', '').replace('/', '')
             
-            # Receive response
-            while True:
-                responseBrowser = s.recv(2 ** 20)
-                if not responseBrowser:
-                    print('Not Response!!!')
-                    break
-                print('Received Response is: ', req.decode().split()[1])
-                print('Contents:', end='\n')
-                print(responseBrowser.decode('utf-8'))
-            s.close()
+            try:
+                fileCache = open('_pycache_/' + cacheName, 'rb')
+                print('Founded ', pathName, 'in cache')
+                
+                responseBrowser = fileCache.read()
+                
+                client.send(responseBrowser)
+                
+            except IOError:
+                # print('Path: ', req.decode().split()[4], end='\n')
 
-    # else:
-    #     print("Validation failed:", error_message[0])
-    #     webbrowser.open_new_tab('error.html') # open 403 when meets the invalid time, link: https://www.guru99.com/accessing-internet-data-with-python.html
-    return
+                addr = req.decode().split()[4]
+                if not addr.startswith('www.'):
+                    addr = 'www.' + addr
+                # print(addr, end='\n')
+                s = socket(AF_INET,SOCK_STREAM)
+                s.connect((addr, 80))
+                # Send request to client
+                s.send(req)
+                
+                # Read type caches
+                typeCache = pathName.split('.')[-1]
+                if typeCache in request_config.cache_types:
+                    cache = open('_pycache_/' + cacheName, 'wb')
+                    caches.append({
+                        'name': cacheName,
+                        'time': datetime.now() + timedelta(minutes = int(request_config.cache_time))                        
+                    })
+                # Receive response
+                while True:
+                    responseBrowser = s.recv(2 ** 20)
+                    if not responseBrowser:
+                        print('Not Response !!!')
+                        break
+                    print('Received Response for: ', req.decode().split()[1])
+                    if typeCache in request_config.cache_types:
+                        header, body = responseBrowser.split(b'\r\n\r\n', 1)
+                        cache.write(body)
+                        
+                    client.send(responseBrowser)
+                    
+                s.close()
+    else:
+        
+        reply = handleForbidden()
+        client.send(reply)
+
+    # Close connect from browser
+    client.close()
+    
+#MAIN
 
 #create a server
 socServer = socket(AF_INET, SOCK_STREAM)
+
+for file in os.listdir('_pycache_'):
+    os.remove('_pycache_/' + file)
+
+caches = []
+cacheThread = Thread(target = releaseCache, args = [caches])
+cacheThread.start()
 
 try:
     socServer.bind(('', 8000))
@@ -148,17 +188,4 @@ print("Listening ...")
 
 while True:
     clientSocket, address = socServer.accept()
-    Thread(target = ProcessProxy, args = [clientSocket]).start()
-    
-'''
-GET http://example.com/ HTTP/1.1
-Host: example.com
-User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/116.0
-Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8
-Accept-Language: en-US,en;q=0.5
-Accept-Encoding: gzip, deflate
-Connection: keep-alive
-Upgrade-Insecure-Requests: 1
-If-Modified-Since: Thu, 17 Oct 2019 07:18:26 GMT
-If-None-Match: "3147526947"
-'''
+    Thread(target = ProcessProxy, args = [clientSocket, caches]).start()
